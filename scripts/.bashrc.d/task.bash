5b40b45193626f5d788371339d658cce328acf7b
jcdematos <github@jcdematos.com>
2022-09-10 20:36:56 -0300

    More bash aliases

---
+++ scripts/.bashrc.d/task.bash
@@ -0,0 +1,9 @@
+alias today='clear && task +TODAY'
+alias tomorrow='clear && task +TOMORROW -habit'
+alias week='clear && task +WEEK'
+alias month='clear && task +MONTH'
+alias overdue='task +OVERDUE'
+
+alias in='task add tag:inbox'
+alias toler='task add tag:ler due:someday'
+
