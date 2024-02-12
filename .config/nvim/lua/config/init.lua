00eb13557909f041604f80b600e48eff3f9dbef9
jcdematos <github@jcdematos.com>
2024-02-12 15:09:56 -0300

    neovim configuration

---
+++ .config/nvim/lua/config/init.lua
@@ -0,0 +1,6 @@
+local opt = vim.opt
+
+vim.g.mapleader = " "
+
+opt.number = true
+opt.relativenumber = true
---
+++ .config/nvim/lua/remaps/init.lua
@@ -0,0 +1,16 @@
+vim.g.maplead = " ";
+
+vim.keymap.set("n", "<leader>e", vim.cmd.Explore);
+vim.keymap.set("n", "<leader>t", vim.cmd.terminal);
+
+vim.keymap.set("n", "<leader>bl", vim.cmd.ls);
+vim.keymap.set("n", "<leader>bn", vim.cmd.bnext);
+vim.keymap.set("n", "<leader>bp", vim.cmd.bprevious);
+vim.keymap.set("n", "<leader>bp", vim.cmd.bprevious);
+
+
+function toggle_relativenumber()
+  vim.o.relativenumber = not vim.o.relativenumber
+end
+
+vim.keymap.set("n", "<leader>rr", toggle_relativenumber);
