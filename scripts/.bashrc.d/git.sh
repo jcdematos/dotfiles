fff7ecd96d53bfcbfefb12d679813faac1103b12
jcdematos <github@jcdematos.com>
2024-02-13 15:41:51 -0300

    update git aliases

--- scripts/.bashrc.d/git.sh
+++ scripts/.bashrc.d/git.sh
@@ -1,4 +1,12 @@
+alias gsts='git status'
+alias gl='git log'
+
 alias ga='git add'
 alias gaa='git add .'
+
 alias gm='git commit -m'
+alias gam='git commit -ma'
+
 alias gst='git checkout main && git pull && git checkout -b '
+
+alias gp='git push'
