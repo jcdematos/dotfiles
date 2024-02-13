eb44e96302f16bd1ff6d6ae55af3fbd8303edfbe
jcdematos <github@jcdematos.com>
2024-02-13 15:41:29 -0300

    update tmux startup script

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -2,5 +2,5 @@ session="sync"
 tmux new-session -d -s $session
 
 window=0
-tmux rename-window -t $session:$window 'synthing'
+tmux rename-window -t $session:$window 'syncthing'
 tmux send-keys -t $session:$window 'syncthing' C-m
