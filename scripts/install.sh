66f594a831b86e30daeab3fe5629604a40791d83
jcdematos <github@jcdematos.com>
2025-11-15 15:02:28 -0300

    Update install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,30 +1,35 @@
-# Script used to configure my systems after initial installations 
-# the machines I use at the moment are either fedora or ubuntu
-
-##TODO  Ubuntu install for my other machine
 if [ "$(uname -n)" == "fedora" ]
 then
-	# Remove if installed fedora from KDE flavour
-	sudo rm /etc/dnf/protected.d/plasma.desktop
-	sudo dnf remove @kde-desktop
-	sudo dnf autoremove kamoso kf5-kactivities dragon kasumi elisa-player kolourpaint konversation ark kmines kruler  kpat krdc krfb kmahjongg kcolorchooser kf5-kwallet yelp antiword thai-* nano mariadb gnome-*
-	echo "Enable rpmfusion free"
-	sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+	#sudo rm /etc/dnf/protected.d/plasma.desktop
+	#sudo dnf remove @kde-desktop
+	#sudo dnf autoremove kamoso kf5-kactivities dragon kasumi elisa-player kolourpaint konversation ark kmines kruler  kpat krdc krfb kmahjongg kcolorchooser kf5-kwallet yelp antiword thai-* nano mariadb gnome-*
 
-	echo "Enable copr zeno/scrcpy"
-	sudo dnf copr enable zeno/scrcpy -y
-	echo "Enable copr fuhrmann/i3-gaps"
-	sudo dnf copr enable fuhrmann/i3-gaps -y
+	echo "Enable rpmfusion free"
+	sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
+	echo "Enable rpmfusion nonfree"
+	sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
 	echo "Install packages"
-	sudo dnf install i3-gaps i3blocks git neovim mpv emacs zathura alacritty python3  python3-pip  task picom newsboat firefox krita jq dmenu tmux feh syncthing fontawesome5-fonts adb scrcpy ripgrep khal fd-find nodejs hugo ShellCheck -y
+	sudo dnf install git hyprland hyprutils mako nvim syncthing waybar thunderbird discord -y
+
+	echo "Copying git files"
+	rm -rf ~/.git
+	cp -r .git ~/.git
+	cp -r .gitignore ~
+
+	echo "Copying bash files"
+	cp -r .bash* ~
+	cp -r scripts ~
+	
+	echo "LICENSE and README"
+	cp LICENSE ~
+	cp README.md ~
+
+	echo "Copying config files"
+	cp -r .config ~
+
+	echo "Bin files"
+	cp -r bin ~/bin
 fi
 
-if [ ! -f "$HOME/.emacs.d/bin/doom" ]
-then
-	git clone --depth 1 https://github.com/doomemacs/doomemacs $HOME/.emacs.d
-	~/.emacs.d/bin/doom install
-else
-	~/.emacs.d/bin/doom sync
-fi
 
