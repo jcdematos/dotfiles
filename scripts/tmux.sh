96023074e88b3aa709469b08b4844cbe085c4ad0
jcdematos <github@jcdematos.com>
2025-11-16 17:46:02 -0300

    Update tmux script

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -4,3 +4,15 @@ tmux new-session -d -s $session
 window=0
 tmux rename-window -t $session:$window 'syncthing'
 tmux send-keys -t $session:$window 'syncthing' C-m
+
+window="pypewiring"
+tmux new-window -n $window
+tmux rename-window -t $session:$window 'pypewiring'
+tmux send-keys -t $session:$window 'cd /home/jmatos/github/pypewiring' C-m
+tmux send-keys -t $session:$window 'source venv/bin/activate' C-m
+tmux send-keys -t $session:$window 'python3 main.py' C-m
+
+window="lan-mouse"
+tmux new-window -n $window
+tmux rename-window -t $session:$window 'lan-mouse'
+tmux send-keys -t $session:$window 'lan-mouse daemon' C-m
